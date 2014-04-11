dcf05a4a251172df6118efc6c9e3a307498d0000
Francisco Saldana <lucid@mit.edu>
2014-04-10 23:39:04 -0400

    Wrote johntest

---
+++ johno_testbot.py
@@ -0,0 +1,12 @@
+import rg
+
+class Robot:
+	def act(self, game):
+		available = rg.locs_around(self.location, filter_out = ('invalid', 'obstacle'))
+
+		for loc, bot in game.robots.iteritems():
+			if bot.player_id != self.player:
+				if rg.dist(loc, self.location) <= 5:
+					return ['move', rg.toward(self.location, loc)]
+				else:
+					return ['move', rg.toward(self.location, rg.CENTER_POINT)]
